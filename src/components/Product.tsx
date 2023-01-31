ff5b6beb6a3b029aa537d58bdf1457f56b27537f
= <s4112037@glos.ac.uk>
2023-01-31 22:19:09 +0000

    Improved responsiveness

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,7 +4,7 @@ import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 export function Product() {
 
   return (
-  <div className='w-2/4 bg-white rounded-lg absolute'>
+  <div className='w-2/4 md:w-[540px] lg:w-[856px] bg-white rounded-lg absolute'>
     <div className='flex flex-col md:flex-row justify-between items-center overflow-hidden'>
       
       <div className='bg-cover bg-center object-contain bg-[url("/images/image-product-desktop.jpg")] w-full h-72 md:h-[30em] rounded-t-lg md:rounded-none md:rounded-l-lg'>
@@ -12,11 +12,11 @@ export function Product() {
       <div className='p-6 md:h-[30em]'>
         <h2 className='text-[#7b7e84] text-sm mb-4 uppercase tracking-[0.3em]'>Perfume</h2>
         <h1 className='text-[#1f2428] font-["Fraunces"] font-[700] text-4xl'>Gabrielle Essence Eau De Parfum</h1>
-        <p className='my-8 text-[#919197] text-[14px]'>A floral, solar and voluptuous interpreation composed by Olivier Polge,
+        <p className='my-4 md:my-6 lg:my-8 text-[#919197] text-[14px]'>A floral, solar and voluptuous interpreation composed by Olivier Polge,
           Perfumer-Creator for the House of CHANEL.
         </p>
 
-        <div className='flex flex-row items-center gap-x-6 mb-8'>
+        <div className='flex flex-row items-center gap-x-6 mb-6 md:mb-8'>
           <h3 className='text-[#3a8267] font-["Fraunces"] font-[700] text-3xl'>$149.99</h3>
           <h4 className='line-through text-[#919197]'>$169.99</h4>
         </div>
